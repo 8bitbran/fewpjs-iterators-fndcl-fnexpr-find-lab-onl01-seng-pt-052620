7c3792b339f88f52ed7cb549cdb2de32e0fb6a5f
8bitbran <brandonmar97@gmail.com>
2020-12-13 13:32:12 -0500

    commit tree c69eac5975bfd81051c0ee7035e4f4a1944d7d3b

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function superbowlWin(records) {
-  let win = records.find(o => o.result === "W");
+  // let win = records.find(o => o.result === "W");
+  let win = records.find(function(o) {
+    return o.result === "W"
+  });
   return win ? win.year : undefined;
 }
